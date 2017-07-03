13e8027082ca445b74c9cf4ea30cc6c2c2a8a0ca
masaki <masaki.iima@gmail.com>
2017-07-03 14:56:46 +0900

    feat:page01の実装

--- src/app/page01/page01.component.ts
+++ src/app/page01/page01.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
+
+import { CalcService } from "./calc.service";
+import { CounterService } from "./counter.service";
 
 @Component({
   selector: 'app-page01',
   templateUrl: './page01.component.html',
   styleUrls: ['./page01.component.css']
 })
-export class Page01Component implements OnInit {
 
-  constructor() { }
+export class Page01Component implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  value01 = 0;
 
+  constructor(
+    private calcService: CalcService,
+    private CounterService: CounterService) { 
+      console.log("@@@constructor");
+  }
+
+  calc(): number {
+    console.log("@@@calc");
+    return this.calcService.addTax(this.value01);
+  }
   ngOnInit() {
+    console.log("@@@ngOnInit");
+    this.CounterService.counter++;
+  }
+
+  ngOnChanges(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  ngDoCheck(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  ngAfterContentInit(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  ngAfterContentChecked(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  ngAfterViewInit(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  ngAfterViewChecked(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  ngOnDestroy(): void {
+    throw new Error("Method not implemented.");
   }
 
 }
