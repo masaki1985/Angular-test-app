57be8f07cdf8e23afe28ea1473ef73cf63e5f9d9
masaki <masaki.iima@gmail.com>
2017-07-03 14:11:59 +0900

    feat:page01コンポーネント生成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //HTMLの出力先として<app-root>を指定
   selector: 'app-root',
+
+  //HTMLテンプレートファイルの指定
   templateUrl: './app.component.html',
+
+  //スタイルシートファイルの指定
   styleUrls: ['./app.component.css']
 })
+
+//componentクラスの定義
 export class AppComponent {
-  title = 'app';
+
+  //変数titleの宣言と値の代入
+  title = 'こんにちは';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Page01Component } from './page01/page01.component';
 
 @NgModule({
+  //このアプリで利用するクラス
   declarations: [
-    AppComponent
+    AppComponent,
+    Page01Component
   ],
+  //このアプリで利用するモジュール
   imports: [
     BrowserModule
   ],
+  //DI元となるクラス
   providers: [],
+  //最初に起動するコンポーネント
   bootstrap: [AppComponent]
 })
 export class AppModule { }
