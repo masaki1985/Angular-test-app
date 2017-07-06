3e1ac35bf5f23647a0146e3b41632b1421e05f17
masaki <masaki.iima@gmail.com>
2017-07-06 15:27:57 +0900

    feat:module分割のためファイル生成

---
+++ src/app/page01/page01.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Page01RoutingModule } from './page01-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    Page01RoutingModule
+  ],
+  declarations: []
+})
+export class Page01Module { }
---
+++ src/app/page02/page02.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Page02RoutingModule } from './page02-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    Page02RoutingModule
+  ],
+  declarations: []
+})
+export class Page02Module { }
